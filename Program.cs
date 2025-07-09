91e633e3913761886894517fa753a087318b99ec
LiSai <1747070238@qq.com>
2025-07-09 15:53:14 +0800

    2211

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Win32;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Resources;
@@ -7,14 +8,9 @@ namespace CatTest
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             var procMutex = new Mutex(true, "_RUNCAT_MUTEX", out var result);
             if (!result)
             {
@@ -23,112 +19,127 @@ namespace CatTest
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.Run(new RunCatApplicationContext());
+            procMutex.ReleaseMutex();
         }
     }
     public class RunCatApplicationContext : ApplicationContext
     {
-        private const int CPU_TIMER_DEFAULT_INTERVAL = 3000;
+        private const int CPU_TIMER_DEFAULT_INTERVAL = 2000;
         private const int ANIMATE_TIMER_DEFAULT_INTERVAL = 200;
 
         private PerformanceCounter cpuCounter;
         private NotifyIcon notifyIcon;
-        private string runner = string.Empty;
         private int current = 0;
         private float minCpu;
         private float interval;
-        private string systemTheme = string.Empty;
-        private string manualTheme = UserSetting.Default.Theme;
-        private string speed = UserSetting.Default.Speed;
         private Icon[] icons;
-        private Timer animateTimer = new Timer();
-        private Timer cpuTimer = new Timer();
+        private static bool IsContiniu = true;
 
         public RunCatApplicationContext()
         {
             UserSetting.Default.Reload();
-            runner = UserSetting.Default.Runner;
-            manualTheme = UserSetting.Default.Theme;
             Application.ApplicationExit += new EventHandler(onapplication);
             cpuCounter = new PerformanceCounter("Processor Information", "% Processor Utility", "_Total");
             _ = cpuCounter.NextValue();
-
-            ContextMenuStrip contextMenuStrip = new ContextMenuStrip(new Container());
+            var contextMenuStrip = new ContextMenuStrip(new Container());
             contextMenuStrip.Items.AddRange(new ToolStripItem[]
             {
-                new ToolStripSeparator(),
-                new ToolStripMenuItem($"{Application.ProductName} v{Application.ProductVersion}")
-                {
-                    Enabled = false
-                },
                 new ToolStripMenuItem("Exit", null, Exit)
             });
-
             notifyIcon = new NotifyIcon()
             {
                 Icon = Resource.light_cat_0,
-                ContextMenuStrip = contextMenuStrip,
                 Text = "0.0%",
-                Visible = true
+                Visible = true,
+                ContextMenuStrip=contextMenuStrip
             };
-
-            SetAnimation();
-            StartObserveCPU();
+            notifyIcon.DoubleClick += (sender, e) =>
+            {
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = "powershell",
+                    UseShellExecute = false,
+                    Arguments = " -c Start-Process taskmgr.exe",
+                    CreateNoWindow = true,
+                };
+                Process.Start(startInfo);
+            };
+            var theme = GetAppsUseTheme();
+            string themename = string.Empty;
+            if(theme== "dark")
+            {
+                themename = "dark_cat_";
+            }
+            else
+            {
+                themename = "light_cat_";
+            }
+            ResourceManager rm = Resource.ResourceManager;
+            List<Icon> iconList = new List<Icon>();
+            for (int i = 0; i < 5; i++)
+            {
+                Icon? ic = (Icon)rm.GetObject($"{themename}{i}");
+                if (ic == null)
+                {
+                    continue;
+                }
+                iconList.Add(ic);
+            }
+            icons = iconList.ToArray();
             current = 1;
+            Task.Run(async () => await WhileFoeve());
+            Task.Run(async () => await GetCpu());
+
+
         }
 
+        private string GetAppsUseTheme()
+        {
+            string keyName = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+            using (RegistryKey rKey = Registry.CurrentUser.OpenSubKey(keyName))
+            {
+                object value;
+                if (rKey == null || (value = rKey.GetValue("SystemUsesLightTheme")) == null)
+                {
+                    return "light";
+                }
+                int theme = (int)value;
+                return theme == 0 ? "dark" : "light";
+            }
+        }
         private void onapplication(object? sender, EventArgs e)
         {
-            UserSetting.Default.Speed = speed;
-            UserSetting.Default.Theme = manualTheme;
-            UserSetting.Default.Runner = runner;
-            UserSetting.Default.Save();
+            IsContiniu = false;
         }
 
-        private void SetAnimation()
+        public async Task WhileFoeve()
         {
-            animateTimer.Interval = ANIMATE_TIMER_DEFAULT_INTERVAL;
-            animateTimer.Tick += new EventHandler(AnimationTick);
-        }
-        private void AnimationTick(object sender, EventArgs e)
-        {
-            if (icons.Length <= current) current = 0;
-            notifyIcon.Icon = icons[current];
-            current = (current + 1) % icons.Length;
-        }
-
-        private void StartObserveCPU()
-        {
-            cpuTimer.Interval = CPU_TIMER_DEFAULT_INTERVAL;
-            cpuTimer.Tick += new EventHandler(ObserveCPUTick);
-            cpuTimer.Start();
+            interval = ANIMATE_TIMER_DEFAULT_INTERVAL;
+            while (IsContiniu)
+            {
+                if (icons.Length <= current) current = 0;
+                notifyIcon.Icon = icons[current];
+                current = (current + 1) % icons.Length;
+                await Task.Delay((int)interval);
+            }
         }
 
-        private void ObserveCPUTick(object sender, EventArgs e)
+        public async Task GetCpu()
         {
-            CPUTick();
-        }
-        private void CPUTick()
-        {
-            interval = Math.Min(100, cpuCounter.NextValue()); // Sometimes got over 100% so it should be limited to 100%
-            notifyIcon.Text = $"CPU: {interval:f1}%";
-            interval = 200.0f / (float)Math.Max(1.0f, Math.Min(20.0f, interval / 5.0f));
-            _ = interval;
-            CPUTickSpeed();
-        }
-        private void CPUTickSpeed()
-        {
-            float manualInterval = (float)Math.Max(minCpu, interval);
-            animateTimer.Stop();
-            animateTimer.Interval = (int)manualInterval;
-            animateTimer.Start();
-
+            float a = 0f;
+            while (IsContiniu)
+            {
+                a = Math.Min(100, cpuCounter.NextValue());
+                notifyIcon.Text = $"CPU: {a:f1}%";
+                float manualInterval = 200.0f / (float)Math.Max(1.0f, Math.Min(20.0f, a) / 5.0f);
+                interval = (float)Math.Max(minCpu, manualInterval);
+                await Task.Delay(CPU_TIMER_DEFAULT_INTERVAL);
+            }   
         }
         private void Exit(object sender, EventArgs e)
         {
-            cpuCounter.Close();
-            animateTimer.Stop();
-            cpuTimer.Stop();
-            notifyIcon.Visible = false;
+            IsContiniu = false;
             Application.Exit();
         }
     }
